acf7f7400820797c9bf278773f9eb66ee1eb944d
SarthakNarayan <sarthak.narayang@gmail.com>
2021-11-25 10:57:36 +0530

    added support for different audio formats

--- download.sh
+++ download.sh
@@ -12,10 +12,10 @@ fi
 echo -e "\nFTP server is up downloading the video"
 echo -e "--------------------------------------------------------- \n[`date`]" >> ./logs/download.log
 # yt-dlp -x --restrict-filenames --no-progress -o '%(title)s.%(ext)s' $1 &>> ./logs/download.log
-# only record the errors 
+# only record the errors
 yt-dlp -x --restrict-filenames -o '%(title)s.%(ext)s' $1 2>> ./logs/download.log
-audio_file=`ls | grep .opus` 
-video_name="${audio_file%%.*}" #get the name of the video 
+audio_file=`ls | grep .opus || ls | grep .mp3 || ls | grep .mp4`
+video_name="${audio_file%%.*}" #get the name of the video
 echo "Video: $video_name" >> ./logs/download.log
 if [ $? -eq 0 ]; then
     echo -e "\nNo errors occured while downloading the video" >> ./logs/download.log
@@ -40,7 +40,7 @@ fi
 converted_audio_file=${audio_file}_converted_${audio_rate}.opus
 echo -e "Duration of the original video \n `ffmpeg -i $audio_file 2>&1 | grep Duration`"
 echo -e "--------------------------------------------------------- \n[`date`]" >> ./logs/conversion.log
-ffmpeg -loglevel error -i $audio_file -filter:a "atempo=${audio_rate}" -vn $converted_audio_file > /dev/null 2>> ./logs/conversion.log 
+ffmpeg -loglevel error -i $audio_file -filter:a "atempo=${audio_rate}" -vn $converted_audio_file > /dev/null 2>> ./logs/conversion.log
 echo "Audio: $audio_file" >> ./logs/conversion.log
 if [ $? -eq 0 ]; then
     echo -e "\nNo errors occured while increasing the playback speed" >> ./logs/conversion.log
