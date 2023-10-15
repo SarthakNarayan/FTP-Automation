19032109d030b9f0b53e71568341aaf58e9d9a8a
Sarthak Narayan <sarthak.narayang@gmail.com>
2023-10-15 11:02:48 -0400

    updates

--- code/config.py
+++ code/config.py
@@ -2,5 +2,5 @@ PASSWORD = "android"
 USERNAME = "android"
 LOGGING_LOCATION = "logs/logging.log"
 IP = "192.168.4.43"
-# IP = "100.71.205.12"
+# IP = "100.71.202.37"
 PORT = 2221
--- download.py
+++ download.py
@@ -23,56 +23,66 @@ if links == []:
     print("links.txt is empty exiting")
     sys.exit(1)
 
+error_links = []
+
 cmd("mkdir -p logs")
+cmd("mkdir -p Downloads")
 print("Checking if FTP server is UP...")
 
-status_code = cmd("python3 code/CheckFtpConnection.py").returncode
-if status_code != 0:
-    print("FTP server is down please try again after restarting the server")
-    sys.exit(1)
+while links != []:
+    link = links[0]
+    try:
+        print()
+        print(10 * "-")
+        print("⬇️  Starting Video Download...")
+        status_code = cmd(
+            f"yt-dlp -x --restrict-filenames -o '%(title)s.%(ext)s' {link} 2>>./logs/download.log"
+        ).returncode
 
-print("⬆️ FTP server is up downloading the videos")
-for link in links:
-    print()
-    status_code = cmd(
-        f"yt-dlp -x --restrict-filenames -o '%(title)s.%(ext)s' {link} 2>>./logs/download.log"
-    ).returncode
+        if status_code != 0:
+            print(
+                f"Some error has occured while downloading the video {link} refer download logs"
+            )
+            raise Exception
+        time.sleep(3)
 
-    if status_code != 0:
-        print(
-            f"Some error has occured while downloading the video {link} refer download logs"
+        audio_file = cmd(
+            "ls | grep .opus || ls | grep .mp3 || ls | grep .mp4 || ls | grep .m4a",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        ).stdout
+
+        if audio_file == "":
+            print(
+                "Variable audio_file is empty, this may be due to unknown audio format. Please remove the downloaded video"
+            )
+            raise Exception
+
+        audio_file = audio_file.replace("\n", "")
+        converted_audio_file = f"{audio_file}_converted_{AUDIO_RATE}.mp3"
+
+        print(f"📋 Name of the video: {audio_file}")
+        print("🔊 Video download complete changing the rate of the audio file.")
+        cmd(
+            f'ffmpeg -loglevel error -i {audio_file} -filter:a "atempo={AUDIO_RATE}" -vn {converted_audio_file} >/dev/null 2>>./logs/conversion.log'
         )
-        sys.exit(1)
-    time.sleep(3)
+        print("⬆️  Audio converision complete. Starting upload...")
+        status_code = cmd(f"mv {converted_audio_file} ./recordings").returncode
+        if status_code != 0:
+            print(
+                "Some error occurred while transferring the file, please check logs, cleaning up"
+            )
+            cmd(f"rm {audio_file}")
+            raise Exception
 
-    audio_file = cmd(
-        "ls | grep .opus || ls | grep .mp3 || ls | grep .mp4 || ls | grep .m4a",
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    ).stdout
-
-    if audio_file == "":
-        print(
-            "Variable audio_file is empty, this may be due to unknown audio format. Please remove the downloaded video"
-        )
-        sys.exit(1)
-
-    audio_file = audio_file.replace("\n", "")
-    converted_audio_file = f"{audio_file}_converted_{AUDIO_RATE}.opus"
-
-    cmd(
-        f'ffmpeg -loglevel error -i {audio_file} -filter:a "atempo={AUDIO_RATE}" -vn {converted_audio_file} >/dev/null 2>>./logs/conversion.log'
-    )
-    print("Audio converision complete. Starting upload...")
-    status_code = cmd(
-        f"python3 code/Uploader.py --file {converted_audio_file}"
-    ).returncode
-    if status_code != 0:
-        print(
-            "Some error occurred while transferring the file, please check logs, cleaning up"
-        )
+        print("🗑️  File successfully moved file to Downloads, doing cleanup")
         cmd(f"rm {audio_file}")
-        sys.exit(1)
+        links.pop(0)
+    except Exception as e:
+        print(f"Exception: {e}")
+        cmd("trash *.part *.ytdl", check=False)
 
-    print("File successfully uploaded, doing cleanup")
-    cmd(f"rm {audio_file} {converted_audio_file}")
+cmd("echo '' > links.txt")
+cmd(
+    "mv ./recordings/* '/Users/sarthaknarayan/Library/Mobile Documents/com~apple~CloudDocs/Recordings/'"
+)
